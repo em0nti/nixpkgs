98cfaa92ac2c6f3f3f63085096b4e3fff5981b1b
em0nti <39101979+em0nti@users.noreply.github.com>
2025-04-11 17:27:33 +0300

    clean up zed-editor settings

--- home.nix
+++ home.nix
@@ -19,7 +19,7 @@
     command = ${pkgs.fish}/bin/fish --login --interactive
     shell-integration = fish
     font-size = 16
-'';
+  '';
 
   programs = {
     home-manager.enable = true;
@@ -30,49 +30,53 @@
     fish = {
       enable = true;
       interactiveShellInit = ''
-    # Add Homebrew to PATH
-    if test -d /opt/homebrew/bin
-      fish_add_path /opt/homebrew/bin
-      fish_add_path /opt/homebrew/sbin
-    else if test -d /usr/local/bin
-      fish_add_path /usr/local/bin
-      fish_add_path /usr/local/sbin
-    end
+        # Add Homebrew to PATH
+        if test -d /opt/homebrew/bin
+          fish_add_path /opt/homebrew/bin
+          fish_add_path /opt/homebrew/sbin
+        else if test -d /usr/local/bin
+          fish_add_path /usr/local/bin
+          fish_add_path /usr/local/sbin
+        end
 
-    # Set Homebrew environment variables
-    set -gx HOMEBREW_PREFIX "/opt/homebrew"
-    set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar"
-    set -gx HOMEBREW_REPOSITORY "/opt/homebrew"
-    '';
-    };  
-zed-editor = {
-enable = true;
-extensions = [ "nix" "toml" "elixir" "make" ];
-userSettings = {
-features = {
-edit_prediction_provider = "copilot";
+        # Set Homebrew environment variables
+        set -gx HOMEBREW_PREFIX "/opt/homebrew"
+        set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar"
+        set -gx HOMEBREW_REPOSITORY "/opt/homebrew"
+      '';
     };
-    assistant = {
-      default_model = {
-        provider = "zed.dev";
-        model = "claude-3-7-sonnet-latest";
+    zed-editor = {
+      enable = true;
+      extensions = [
+        "nix"
+        "html"
+        "xml"
+        "mermaid"
+        "workflow-description-language"
+      ];
+      userSettings = {
+        features = {
+          edit_prediction_provider = "copilot";
+        };
+        assistant = {
+          default_model = {
+            provider = "zed.dev";
+            model = "claude-3-7-sonnet-latest";
+          };
+          version = "2";
+        };
+        base_keymap = "VSCode";
+        ui_font_size = 16;
+        buffer_font_size = 16;
+        theme = {
+          mode = "system";
+          light = "Tokyo Night Light";
+          dark = "Tokyo Night";
+        };
       };
-      version = "2";
+      userKeymaps = { };
+      installRemoteServer = false;
     };
-    base_keymap = "VSCode";
-    ui_font_size = 16;
-    buffer_font_size = 16;
-    theme = {
-      mode = "system";
-      light = "Tokyo Night Light";
-      dark = "Tokyo Night";
-    };
-  };
-  userKeymaps = {
-    "ctrl-s" = "workspace::Save";
-  };
-  installRemoteServer = false;
-};
   };
 
   # State version - don't change this
